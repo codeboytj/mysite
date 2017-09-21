66474ba015355c1debcd6f61095896aaeeb6aa32
tj <2238010512@qq.com>
2017-09-21 11:50:13 +0800

    管理页面

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,33 @@
+# coding=utf-8
 from django.contrib import admin
-from .models import Question
+from .models import Choice,Question
+
+# Question的关联对象Choice，
+# class ChoiceInline(admin.StackedInline):
+# 使用TabularInline代替StackedInline让关联对象的排列变成表格形式的，更加紧凑
+class ChoiceInline(admin.TabularInline):
+    # 关联的模型
+    model = Choice
+    # 默认获得3个空白Choice
+    extra = 3
+
 
 # Register your models here.
+# 自定义管理表单
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        # 把字段分为多个字段集，也可以通过改变先后顺序来指定html中的字段顺序
+        (None,{'fields':['question_text']}),
+        # 同时，还可以通过classes指定相应的字段集的html样式
+        ('Data Information',{'fields':['pub_date'],'classes':['collapse']}),
+    ]
+    # 设置关联对象
+    inlines = [ChoiceInline]
+    # 通过list_display自定义question管理页面中显示的question的字段
+    list_display = ('question_text','pub_date','was_published_recently')
+    # 过滤器，会出现在question的管理页面中，Djang根据pub_date的类型给出相应选项
+    list_filter = ['pub_date']
+    # 在管理页面添加按照text搜索question的功能
+    search_fields = ['question_text']
 
-
+admin.site.register(Question,QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -1,15 +1,32 @@
 # coding=utf-8
+import datetime
+
 from django.db import models
+from django.utils import timezone
+# from django.utils.datetime_safe import datetime
 
 # Create your models here.
 
 # 模型（M），对应数据库里面的表
+
+
 class Question(models.Model):
     # 模型的属性，对应数据库表格里面的字段
     # 问题内容
     question_text=models.CharField(max_length=200)
     # 发布时间
     pub_date=models.DateTimeField('date published')
+
+    # 是否近期发布
+    def was_published_recently(self):
+        return self.pub_date>=timezone.now() - datetime.timedelta(days=1)
+
+    # 这句话效果是：当管理页面点击was_published_recently进行排序的时候，管理页面按照pub_date字段进行排序
+    was_published_recently.admin_order_field='pub_date'
+    # 没搞懂这代码是干啥的呢？
+    was_published_recently.boolean=True
+    # 初始的时候管理页面的该列标题为'was_published_recently'，这样设置之后就变成了'Published recently'
+    was_published_recently.short_description='Published recently'
 
 class Choice(models.Model):
     question=models.ForeignKey(Question)
