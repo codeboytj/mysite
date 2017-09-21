865fa44546e6bf8b591e4c6228249cea2cc9202d
tj <2238010512@qq.com>
2017-09-21 17:21:04 +0800

    表单交互

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 # coding=utf-8
-from django.shortcuts import render
-from django.http import HttpResponse,Http404
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.template import RequestContext,loader
-from .models import Question
+from django.core.urlresolvers import reverse
+from .models import Question,Choice
 
 # Create your views here.
 
@@ -31,10 +32,28 @@ def detail(request,question_id):
 
 # 投票结果视图
 def results(request,question_id):
-    response="You're looking at the result of question %s."
-    return HttpResponse(response % question_id)
+
+    question=Question.objects.get(pk=question_id)
+    return render(request,'polls/results.html',{'question':question})
 
 # 投票视图
 def vote(request,question_id):
-    response="You're voting on question %s."
-    return HttpResponse(response % question_id)
+    # get_object_or_404()为raise Http404的快捷方式，如果有相应的question，则获得相应对象，如果没有则返回404
+    p=get_object_or_404(Question,pk=question_id)
+
+    try:
+        # 根据post传过来的名为choice的radio标签传过来的id参数，找到相应choice
+        selected_choice=p.choice_set.get(pk=request.POST['choice'])
+    except(KeyError,Choice.DoesNotExist):
+        # 投票出错，以error_message返回错误信息，显示在detail页面上
+        return render(request,'polls/detail.html',{
+            'question':p,
+            'error_message':"You didn't select a choice"
+        })
+    else:
+        # 得票数自增,通过save方法改动到数据库
+        selected_choice.votes+=1;
+        selected_choice.save()
+        # 投票成功，返回到投票结果页面
+        # reverse()函数使用url中配置的视图名作为参数，避免了使用硬编码，之后设置的args作为参数传给相应的url中的匹配
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
