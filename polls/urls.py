18235ab47a9281723e55eda779583e30e5a6bbfb
tj <2238010512@qq.com>
2017-09-21 17:44:24 +0800

    改良视图

--- polls/urls.py
+++ polls/urls.py
@@ -5,11 +5,14 @@ from . import views
 # polls应用中的url配置
 urlpatterns=[
     # 将符合正则表达式'^$'的url匹配views.py中设置的index视图,polls/
-    url(r'^$',views.index,name='index'),
+    # url(r'^$',views.index,name='index'),
+    url(r'^$',views.IndexView.as_view(),name='index'),
     # 匹配带参数的详情视图,P<question_id>定义了传入view.detail中的匹配的参数名字，polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$',views.detail,name='detail'),
+    # url(r'^(?P<question_id>[0-9]+)/$',views.detail,name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name='detail'),
     # 匹配投票结果视图,polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$',views.results,name='results'),
+    # url(r'^(?P<question_id>[0-9]+)/results/$',views.results,name='results'),
+    url(r'^(?P<pk>[0-9]+)/results/$',views.ResultsView.as_view(),name='results'),
     # 匹配投票视图,polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$',views.vote,name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,38 +3,35 @@ from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.template import RequestContext,loader
 from django.core.urlresolvers import reverse
+# 引入django的通用视图
+from django.views import generic
 from .models import Question,Choice
 
 # Create your views here.
 
-def index(request):
-    # 配置index页面视图
-    # 获取最新的5个question
-    latest_question_list=Question.objects.order_by('-pub_date')[:5]
-    # 载入polls/index.html模板
-    template=loader.get_template('polls/index.html')
-    context=RequestContext(request,{
-        'latest_question_list':latest_question_list,
-    })
-    # 返回给浏览器由模板渲染出来的视图
-    return HttpResponse(template.render(context))
+# 改良视图
+class IndexView(generic.ListView):
+
+    template_name='polls/index.html'
+    # 传入传页面中的对象列表的参数名
+    # 然后generic.ListView会自动使用question_list，所以需要使用context_object_name进行更改以与页面进行匹配
+    context_object_name='latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 # 接受参数的视图
 # 问题详情页面
-def detail(request,question_id):
-    try:
-        question=Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        # 没有找打相应的问题返回Http404的视图
-        raise Http404("Question does not exist")
-    # render的快捷使用方法，传入request，模板的名称，一个字典（相当于之前往context里面传入的字典）
-    return render(request,'polls/detail.html',{'question':question})
+class DetailView(generic.DetailView):
+    # generic.DetailView这个类从url中获得名为'pk'的主键值，所以需要在urls.py中更改question_id为pk
+    # 然后generic.DetailView会自动根据Question，生成名为question的模型加入context
+    model = Question
+    template_name = 'polls/detail.html'
 
 # 投票结果视图
-def results(request,question_id):
-
-    question=Question.objects.get(pk=question_id)
-    return render(request,'polls/results.html',{'question':question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 # 投票视图
 def vote(request,question_id):
